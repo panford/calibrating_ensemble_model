d002fb980ba32a2779d0f67a53905ff28606cdac
Kobby Panford-Quainoo <panfordkobby@gmail.com>
2021-11-17 17:48:39 +0000

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -1,108 +1,88 @@
+import torch
+from torch import nn as nn
 import numpy as np
-import sklearn
-from sklearn.calibration import calibration_curve
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-class calibrator():
-  def __init__(self, class_type='binary', n_bins=10):
-    self.class_type = class_type
-    self.n_bins = None
-    self.results_dict = {}
 
-    if class_type == "binary":
-      results_dict['ece'] = self.expected_cal_error_two_classes()
-      results_dict['mce'] = self.maximimum_cal_error_two_classes()
-      results_dict['rmsce'] = self.rmsce_cal_error_two_classes()
 
-    elif class_type == 'multiclass':
-      results_dict['ece'] = self.expected_cal_error_multiclass()
-      results_dict['mce'] = self.maximimum_cal_error_multiclass()
-      results_dict['rmsce'] = self.rmsce_cal_error_multiclass()
+class Calibration(nn.Module):
+  def __init__(self, num_bins, prob_range, num_classes=2):
+    self.num_bins = num_bins
+    self.prob_range = prob_range
 
-    self.dataframe = None
+  def get_accuracy_confidence(self, probabilities, predictions, targets, eps = 10e-4):
 
-  def set_target_prediction(self, y_true, y_pred):
-    self.y_true = y_true
-    self.y_pred = y_pred
-
-  def get_prob_scores(self, y_true, y_pred):
-    return calibration_curve(y_true, y_pred, self.n_bins)
-
-  def expected_cal_error_two_classes(self, true_probs, pred_probs, bin_ranges):
-    ecetc = 0
-    for bs in np.arange(len(bin_sizes)):
-      ecetc += (bin_ranges[bs] / sum(bin_ranges)) * np.abs(prob_true[bs] - prob_pred[bs])
-    return ecetc
+  
+    _, bin_edges = torch.histogram(probabilities, bins=self.num_bins, range=self.prob_range)
     
+    bin_idx = torch.bucketize(probabilities, bin_edges)
+    bin_counts = torch.bincount(bin_idx, minlength = self.num_bins).add(eps)
 
-  def maximimum_cal_error_two_classes(self, prob_true, prob_pred, bin_ranges):
-    mcetc = 0
-    for bs in np.arange(len(bin_ranges)):
-        mcetc = max(mcetc, np.abs(prob_true[bs] - prob_pred[bs]))
-    return mcetc
+    confidence_vector = bin_idx.bincount(confidence, self.num_bins).div(bin_counts)
+    accuracy_vector = bin_idx.bincount(targets, self.num_bins).div(bin_counts)
 
-  def rmsce_cal_error_two_classes(self, prob_true, prob_pred, bin_ranges):
-    rmscetc = 0
-    for bs in np.arange(len(bin_ranges)):
-        rmscetc += (bin_ranges[bs] / sum(bin_ranges)) * (prob_true[bs] - prob_pred[bs]) ** 2
-    return np.sqrt(rmsce)
+    return accuracy_vector, confidence_vector, bin_counts
+  
+
+  def Binary_ECE(self, probabilities, predictions, targets, norm=1): 
+    """
+    Expected Calibration Error for Binary class 
+    """
+
+    assert norm == 1 or norm == 2, "norm should be either 1 or 2"
+    accuracies, confidences, bin_counts = self.get_accuracy_confidence(probabilities, predictions, targets)
+    acc_conf_diff = accuracies - confidences
+    coeffs = bin_counts/ len(probabilities)
+    scaled_diff = coeffs * acc_conf_diff
+    normed_diff = torch.linalg.norm(scaled_diff, norm)
+    return np.sum(np.abs(normed_diff.numpy()))
 
 
-  def expected_cal_error_multiclass(self, y_true, y_pred):
+  def Binary_MCE(self, probabilities, predictions, targets):
+    """
+    Maximum Calibration Error for Binary class
+    """
+    accuracies, confidences, _ = self.get_accuracy_confidence(probabilities, predictions, targets)
+    acc_conf_diff = torch.abs(accuracies - confidences)
+    return np.amax(acc_conf_diff.numpy(), axis=0).item()
 
-    ece_bin = []
-    for a_class in range(y_true.shape[1]):
-      prob_true, prob_pred = calibration_curve(y_true[a_class], y_pred[a_class], n_bins=10)
-      bin_sizes = np.histogram(a=y_pred[a_class], range=(0, 1), bins=len(prob_true))[0]
-      ece_bin.append(self.ece_calculation_binary(prob_true, prob_pred, bin_sizes))
+  def Binary_RMSCE(self, probabilities, predictions, targets):
+    """Binary Root Mean Squared Calibration Error"""
+    accuracies, confidences, bin_counts = self.get_accuracy_confidence(probabilities, predictions, targets)
+    acc_conf_diff = np.square(accuracies - confidences)
+    coeffs = bin_counts/ len(probabilities)
+    scaled_diff = coeffs * acc_conf_diff
+    return np.sqrt(np.sum(acc_conf_diff.numpy())).item()
 
-    return np.mean(ece_bin)
-          
-      
-  def maximimum_cal_error_multiclass(self, y_true, y_pred):
-    mce_bin = []
-    for a_class in range(y_true.shape[1]):
-      prob_true, prob_pred = calibration_curve(y_true[a_class], y_pred[a_class], n_bins=10)
-      bin_sizes = np.histogram(a=y_pred[a_class], range=(0, 1), bins=len(prob_true))[0]
-      mce_bin.append(self.mce_calculation_binary(prob_true, prob_pred, bin_sizes))
 
-    return np.mean(mce_bin)
-      
-  def rmsce_cal_error_multiclass(self, y_true, y_pred):
-    rmsce_bin = []
-    for a_class in range(y_true.shape[1]):
-        prob_true, prob_pred = calibration_curve(y_true[a_class], y_pred[a_class], n_bins=10)
-        bin_sizes = np.histogram(a=y_pred[a_class], range=(0, 1), bins=len(prob_true))[0]
-        rmsce_bin.append(self.rmsce_calculation_binary(prob_true, prob_pred, bin_sizes))
-    return np.mean(rmsce_bin)
+  # def Multiclass_ECE(self, probabilities, predictions, targets):
+  #   return 
 
-  # calculate the values of calibration curve for bin 0 vs all
-  prob_true_binary, prob_pred_binary = calibration_curve(y_val_binary, y_pred_binary, n_bins=10)
 
-  def plot_reliability_diagram(self, y_true, y_pred, model_name, ax=None):
-    prob_true, prob_pred = self.get_prob_scores(y_true, y_pred)
-    # Plot the calibration curve for ResNet in comparison with what a perfectly calibrated model would look like
+  def reliability_diagram(self, probabilities, predictions, targets):
+    accuracies, confidences, _ = self.get_accuracy_confidence(probabilities, predictions, targets)
+    plt.plot(confidences, accuracies)
+
+  def reliability_diagram(self, probabilities, predictions, targets, model_name, ax=None):
+    # prob_true, prob_pred = self.get_prob_scores(y_true, y_pred)
     
+    accuracies, confidences, _ = self.get_accuracy_confidence(probabilities, predictions, targets)
     if ax==None:
-        fig = plt.figure(figsize=(10, 10))
+        fig = plt.figure(figsize=(6, 6))
         ax = plt.gca()
     else:
         plt.sca(ax)
     
-    plt.plot([0, 1], [0, 1], color="#FE4A49", linestyle=":", label="Perfectly calibrated model")
-    plt.plot(prob_pred, prob_true, "s-", label=model_name, color="#162B37")
+    plt.plot([0, 1], [0, 1], color="#FE4A49", linestyle=":", label="Perfect calibration")
+    plt.plot(accuracies, confidences, "s-", label=model_name, color="#162B37")
 
-    plt.ylabel("Fraction of positives", fontsize=16)
-    plt.xlabel("Mean predicted value", fontsize=16,)
+    plt.ylabel("Accuracies", fontsize=12)
+    plt.xlabel("Confidences", fontsize=12,)
 
-    plt.legend(fontsize=16)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
+    plt.legend(fontsize=12)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
 
     plt.grid(True, color="#B2C7D9")
 
     plt.tight_layout()
-
-  # plot_reliability_diagram(prob_true_binary, prob_pred_binary, "ResNet (class 0 vs all)")
-  #     
-
